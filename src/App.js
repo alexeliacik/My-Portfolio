03865b5858a3bd311917ca76c488c1d282570924
alexeliacik <alexeliacik@gmail.com>
2020-10-29 22:01:45 -0400

    first commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from "react"
+import styled from "styled-components"
+
+const StyledApp = styled.div`
+  display: flex;
+  justify-content: center;
+  background: red;
+  margin: 0;
+`
+
+function App() {
+  return <StyledApp>Hello World</StyledApp>
+}
+
+export default App
