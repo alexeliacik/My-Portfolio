251cba5721e6c8244a248330089153f93bb83c71
alexeliacik <alexeliacik@gmail.com>
2020-11-02 14:03:59 -0500

    some changes

--- src/App.js
+++ src/App.js
@@ -1,15 +1,40 @@
-import React from "react"
-import styled from "styled-components"
+import React from "react";
+import styled, { createGlobalStyle } from "styled-components";
+
+const GlobalStyle = createGlobalStyle`
+* {
+
+  
+  margin:0;
+  padding:0;
+  }
+  
+  *,*::before, *::after {
+    box-sizing: inherit
+  }
+
+  html {
+    font-size: 63.5%;
+    box-sizing: border-box;
+  }
+`;
 
 const StyledApp = styled.div`
+  padding: 0;
+  margin: 0;
   display: flex;
   justify-content: center;
   background: red;
   margin: 0;
-`
+`;
 
-function App() {
-  return <StyledApp>Hello World</StyledApp>
-}
+const App = () => {
+  return (
+    <StyledApp>
+      <GlobalStyle />
+      Hello World
+    </StyledApp>
+  );
+};
 
-export default App
+export default App;
